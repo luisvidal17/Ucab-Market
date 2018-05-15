c3623e46e522a7179fa5b4ebe045e0a0bad582a3
Luis Boada <luisvidal96@hotmail.com>
2018-05-14 20:31:38 -0430

    intent de la second activity a la main activity

--- app/src/main/java/com/example/luisvidal/ucabmarket/SecondActivity.java
+++ app/src/main/java/com/example/luisvidal/ucabmarket/SecondActivity.java
@@ -1,14 +1,24 @@
 package com.example.luisvidal.ucabmarket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
 
 public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.example.luisvidal.ucabmarket.extra.REPLY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+    }
+
+    public void returnReply(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity (intent);
     }
 }
